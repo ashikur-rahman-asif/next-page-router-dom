0e911f860139864a386c2c1d8947e5e6e0ccc21d
asif247 <86229363+asif247@users.noreply.github.com>
2023-04-10 02:31:46 +0600

    using loader data

--- src/assets/components/Books.jsx
+++ src/assets/components/Books.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const Books = () => {
+    // const [books, setBooks] = useState([])
+    // useEffect(() => {
+    //     fetch('https://api.itbook.store/1.0/new')
+    //         .then(res => res.json())
+    //     .then(data=>console.log(data))
+    // },[])
+    const booksData=useLoaderData()
     return (
         <div>
-            <h1>here is books</h1>
+            
         </div>
     );
 };
